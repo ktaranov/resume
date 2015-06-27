8742ef2f8905cedb3b0dfcc6c2903fb0497430bd
shawnajroberts <shawna@automattic.com>
2015-06-26 22:16:41 -0500

    fixed the rest of the mobile styling

--- stickybar.js
+++ stickybar.js
@@ -1,11 +1,13 @@
 var win         = $(window),
     fxel        = $('.navbar-nav'),
     eloffset    = fxel.offset().top;
-
 win.scroll(function() {
-    if (eloffset < win.scrollTop()) {
-        fxel.addClass("fixed");
-    } else {
-        fxel.removeClass("fixed");
-    }
+	if ( $(window).width() > 992) {
+	    if (eloffset < win.scrollTop()) {
+	        fxel.addClass("fixed");
+	    } else {
+	        fxel.removeClass("fixed");
+	    }
+	}
 });
+
