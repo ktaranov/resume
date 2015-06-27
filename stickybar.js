f76d3069105d81864f05ee8284f65a4bbddfd08e
shawnajroberts <shawna@automattic.com>
2015-06-26 19:28:02 -0500

    clean things up

--- stickybar.js
+++ stickybar.js
@@ -1,18 +1,11 @@
 var win         = $(window),
     fxel        = $('.nav'),
-    eloffset    = fxel.offset().top
-    contentarea = $('.mainarea');
-
-console.log(contentarea);
+    eloffset    = fxel.offset().top;
 
 win.scroll(function() {
     if (eloffset < win.scrollTop()) {
         fxel.addClass("fixed");
-        contentarea.addClass("fixed-offset");
-        console.log("added fixed");
     } else {
         fxel.removeClass("fixed");
-        contentarea.removeClass("fixed-offset");
-        console.log("removed fixed");
     }
 });
